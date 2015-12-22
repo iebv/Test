fbef9b4e82b8ad01c76cf9d5b6bf3beff70b5749
Juan Pablo Salamanca <jpsalamarcara@unal.edu.co>
2015-12-22 15:21:48 -0500

    Base de datos enlazada

--- NetBeansProjects/Test/grails-app/domain/test/Book.groovy
+++ NetBeansProjects/Test/grails-app/domain/test/Book.groovy
@@ -10,6 +10,16 @@ class Book {
     String title
     String author
     Double price
+    
+    static mapping = {
+        table name: "LIBRO"
+        version false
+        colummns  {
+            title column : "TITULO"
+            author column: "AUTOR"
+            price column: "PRECIO"
+        }
+    }
 
     static constraints = {
         title blank:false 
