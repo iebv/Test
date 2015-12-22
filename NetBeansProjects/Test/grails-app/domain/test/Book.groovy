3bcf8b96b2ad40dc74f623608fef5689110a233b
snw <snw@snw1-lenovo-g400>
2015-12-22 14:33:59 -0500

    primer commit

---
+++ NetBeansProjects/Test/grails-app/domain/test/Book.groovy
@@ -0,0 +1,18 @@
+package test
+
+import grails.rest.Resource
+
+
+@Resource(uri='/books')
+class Book {
+    
+
+    String title
+    String author
+    Double price
+
+    static constraints = {
+        title blank:false 
+        author blank:false   
+    }
+}
